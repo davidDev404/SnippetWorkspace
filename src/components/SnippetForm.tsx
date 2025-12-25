4852e877155a21ecc3cf7739e95866eb14c7a79e
davidDev404 <cosas.random.para.siempre@gmail.com>
2025-12-24 20:11:03 -0700

    feat(snippet-form): add file creation functionality

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,8 +1,29 @@
+import { writeTextFile } from '@tauri-apps/plugin-fs'
+import { desktopDir } from '@tauri-apps/api/path'
+import { useState } from 'react'
+
 function SnippetEditor(){
+	const [snippetName, setSnippetName] = useState('')
+
 	return (
-		<div>
-			Hello, World!
-		</div>
+		<form onSubmit={async (e) => {
+			e.preventDefault()
+			//alert("Form submitted!")
+			const desktopPath = await desktopDir()
+			//console.log(desktopPath)
+			await writeTextFile(`${desktopPath}/taurifiles/${snippetName}.md`, "")
+			setSnippetName('')
+		}}>
+			<input type="text"
+				placeholder="Write a snippet" 
+				className="bg-zinc-900 w-full border-none outline-none p-4"
+				onChange={(e) => setSnippetName(e.target.value)}
+				value={snippetName}
+			/>
+			<button className="hidden">
+				Save
+			</button>
+		</form>
 	)
 }
 
